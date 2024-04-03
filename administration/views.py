6ef8afa850755d5f3940e88d7e77bc4397456ad9
WhiteLion03 <sergi.baron.pascual@gmail.com>
2024-04-03 18:02:27 +0200

    Edit and delete reservations

--- administration/views.py
+++ administration/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.db.models import QuerySet
 from administration.models import HotelReservation, RestaurantReservation
 from administration.forms import LoginForm, HotelReservationForm, RestaurantReservationForm
 
@@ -49,35 +50,43 @@ def add_reservation(request, reservation_type):
     return render(request, 'add_reservation_form.html', {'reservation_type': reservation_type, 'form': form})
 
 
+@login_required
 def reservation_detail(request, reservation_type, reservation_id):
     if reservation_type == 'hotel':
         model = get_object_or_404(HotelReservation, pk=reservation_id)
     else:
         model = get_object_or_404(RestaurantReservation, pk=reservation_id)
-    return render(request, 'reservation_detail.html', {'reservation_type': reservation_type, 'model': model})
+    return render(request, 'reservation_detail.html', {'reservation_type': reservation_type,
+                                                       'reservation_id': reservation_id, 'model': model})
 
 
 @login_required
-def edit_hotel_reservation(request, reservation_id):
-    reservation = HotelReservation.objects.get(pk=reservation_id)  # Cambia RoomReservation por el modelo correcto
+def edit_reservation(request, reservation_type, reservation_id):
+    if reservation_type == 'hotel':
+        reservation = get_object_or_404(HotelReservation, pk=reservation_id)
+        form_model = HotelReservationForm
+    else:
+        reservation = get_object_or_404(RestaurantReservation, pk=reservation_id)
+        form_model = RestaurantReservationForm
     if request.method == 'POST':
-        form = HotelReservationForm(request.POST, instance=reservation)
+        form = form_model(request.POST, instance=reservation)
         if form.is_valid():
             form.save()
-            return redirect('home')  # Cambia 'home' por la URL de la página principal
+            return redirect('reservation_detail', reservation_type=reservation_type, reservation_id=reservation_id)
     else:
-        form = HotelReservationForm(instance=reservation)
-    return render(request, 'edit_HotelReservation.html', {'form': form})
+        form = form_model(instance=reservation)
+    return render(request, 'edit_reservation.html', {'reservation_type': reservation_type,
+                                                     'reservation_id': reservation_id, 'form': form})
 
 
 @login_required
-def edit_restaurant_reservation(request, reservation_id):
-    reservation = RestaurantReservation.objects.get(pk=reservation_id)  # Cambia RoomReservation por el modelo correcto
-    if request.method == 'POST':
-        form = RestaurantReservationForm(request.POST, instance=reservation)
-        if form.is_valid():
-            form.save()
-            return redirect('home')  # Cambia 'home' por la URL de la página principal
+def delete_reservation(request, reservation_type, reservation_id):
+    if reservation_type == 'hotel':
+        reservation = get_object_or_404(HotelReservation, pk=reservation_id)
     else:
-        form = RestaurantReservationForm(instance=reservation)
-    return render(request, 'edit_RestaurantReservation.html', {'form': form})
+        reservation = get_object_or_404(RestaurantReservation, pk=reservation_id)
+    if request.method == 'POST':
+        reservation.delete()
+        return redirect('reservations_list', reservation_type=reservation_type)
+    return render(request, 'delete_reservation.html', {'reservation_type': reservation_type,
+                                                       'reservation_id': reservation_id, 'reservation': reservation})
