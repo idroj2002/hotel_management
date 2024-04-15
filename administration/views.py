407f1b26a9f41120f80076d0ab57488c28944341
jordi <jordiarenasr@gmail.com>
2024-04-15 18:04:03 +0200

    redundant imports deleted

--- administration/views.py
+++ administration/views.py
@@ -1,6 +1,6 @@
 from django.core.exceptions import PermissionDenied
 from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib.auth.decorators import login_required
 from django.db.models import QuerySet
 from administration.models import HotelReservation, RestaurantReservation, CheckIn
 from administration.forms import LoginForm, HotelReservationForm, RestaurantReservationForm, CheckInForm
