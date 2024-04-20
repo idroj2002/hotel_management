513a4e3f7a7d2bb92711f55740b5f1fba2f31ee0
jordi <jordiarenasr@gmail.com>
2024-04-20 13:45:37 +0200

    cleaning rooms list and cleaning rooms detail merged in one html

--- cleaning/views.py
+++ cleaning/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
 from django.contrib.auth.decorators import login_required
 from administration.models import Room, HotelReservation
