c424c6c5c0739a3cce0e04831af61e71bbf9169d
jordi <jordiarenasr@gmail.com>
2024-05-10 00:33:59 +0200

    accounting template of views

--- accounting/urls.py
+++ accounting/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls.i18n import i18n_patterns
-from accounting.views import accounting_home
+from accounting.views import accounting_home, bills_home, taxes_home, offers_home
 
 urlpatterns = [
     path('', accounting_home, name='accounting_home'),
+    path('bills/', bills_home, name='bills'),
+    path('taxes/', taxes_home, name='taxes'),
+    path('offers/', offers_home, name='offers'),
 ]
